ab9911379b8bc098f830b9778653a08b9f5cedaa
geospiza <70861751+geospiza-fortis@users.noreply.github.com>
2021-06-29 23:34:07 -0700

    Add svelte components for canvas and app (#4)
    * Add svelte components for canvas
    
    * Bump version to 1.6.0

--- src/client/gameloop.js
+++ src/client/gameloop.js
@@ -1,61 +1,48 @@
 import Timer from "./timer";
-import GameCanvas from "./gamecanvas";
 import Camera from "./camera";
 import StateManager from "./statemanager";
 
-const REQUEST_ANIMATION_FRAME = (() => {
-  return (
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    function (callback) {
-      setTimeout(() => {
-        callback && callback();
-      }, 1000 / 60);
-    }
-  );
-})();
-
-const GameLoop = {
-  fps: 60,
-  msPerTick: 1000 / 60,
-  lag: 0,
-};
-
-GameLoop.doUpdate = function (msPerTick, camera, canvas) {
-  StateManager.doUpdate(msPerTick, camera, canvas);
-};
-
-GameLoop.doRender = function (canvas, camera, lag, msPerTick, tdelta) {
-  canvas.drawRect({ x: 0, y: 0, width: 800, height: 600, color: "#000000" });
-  StateManager.doRender(canvas, camera, lag, msPerTick, tdelta);
-};
-
-GameLoop.postRender = function () {
-  GameCanvas.resetMousewheel();
-};
-
-GameLoop.gameLoop = function (highResTimestamp) {
-  REQUEST_ANIMATION_FRAME(GameLoop.gameLoop);
-
-  Timer.update();
-  GameLoop.lag += Timer.delta;
-  while (GameLoop.lag >= GameLoop.msPerTick) {
-    GameLoop.lag -= GameLoop.msPerTick;
-    Timer.tdelta += GameLoop.msPerTick;
-    // TODO: fix ordering of these variables
-    GameLoop.doUpdate(GameLoop.msPerTick, Camera, GameCanvas);
+class GameLoop {
+  fps = 60;
+  msPerTick = 1000 / 60;
+  lag = 0;
+  constructor(gameCanvas) {
+    this.gameCanvas = gameCanvas;
   }
-  GameLoop.doRender(
-    GameCanvas,
-    Camera,
-    GameLoop.lag,
-    GameLoop.msPerTick,
-    Timer.tdelta
-  );
-  GameLoop.postRender();
-};
+
+  doUpdate(msPerTick, camera, canvas) {
+    StateManager.doUpdate(msPerTick, camera, canvas);
+  }
+
+  doRender(canvas, camera, lag, msPerTick, tdelta) {
+    canvas.drawRect({ x: 0, y: 0, width: 800, height: 600, color: "#000000" });
+    StateManager.doRender(canvas, camera, lag, msPerTick, tdelta);
+  }
+
+  postRender(canvas) {
+    canvas.resetMousewheel();
+  }
+
+  gameLoop(highResTimestamp) {
+    requestAnimationFrame(() => this.gameLoop());
+
+    Timer.update();
+    this.lag += Timer.delta;
+    while (this.lag >= this.msPerTick) {
+      this.lag -= this.msPerTick;
+      Timer.tdelta += this.msPerTick;
+      // TODO: fix ordering of these variables
+      this.doUpdate(this.msPerTick, Camera, this.gameCanvas);
+    }
+    this.doRender(
+      this.gameCanvas,
+      Camera,
+      this.lag,
+      this.msPerTick,
+      Timer.tdelta
+    );
+    this.postRender(this.gameCanvas);
+  }
+}
 
 export default GameLoop;
--- src/client/main.js
+++ src/client/main.js
@@ -1,31 +1,8 @@
-import GameLoop from "./gameloop";
-import Timer from "./timer";
-import GameCanvas from "./gamecanvas";
-import WZManager from "./wzmanager";
-import Camera from "./camera";
-import MySocket from "./mysocket";
-import StateManager from "./statemanager";
-import LoginState from "./loginstate";
+import App from "./App.svelte";
 
-async function startGame() {
-  GameCanvas.initialize();
-  GameCanvas.drawRect({
-    x: 0,
-    y: 0,
-    width: 800,
-    height: 600,
-    color: "#000000",
-  });
+const app = new App({
+  target: document.body,
+  props: {},
+});
 
-  StateManager.initialize();
-  WZManager.initialize();
-  Camera.initialize();
-  Timer.initialize();
-  await MySocket.initialize();
-  await StateManager.setState(LoginState);
-  await LoginState.enterGame();
-
-  GameLoop.gameLoop();
-}
-
-startGame();
+export default app;
